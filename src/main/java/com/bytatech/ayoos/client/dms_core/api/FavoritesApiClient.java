5e79bfe5766a967a4fdf0c54bc1357e1c581abd6
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-10-04 12:20:57 +0530

    4commit

---
+++ src/main/java/com/bytatech/ayoos/client/dms_core/api/FavoritesApiClient.java
@@ -0,0 +1,8 @@
+package com.bytatech.ayoos.client.dms_core.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import com.bytatech.ayoos.client.dms_core.ClientConfiguration;
+
+@FeignClient(name="${dmsCore.name:dmsCore}", url="${dmsCore.url:http://localhost/alfresco/api/-default-/public/alfresco/versions/1}", configuration = ClientConfiguration.class)
+public interface FavoritesApiClient extends FavoritesApi {
+}
--- src/main/java/com/bytatech/ayoos/web/rest/CommandResource.java
+++ src/main/java/com/bytatech/ayoos/web/rest/CommandResource.java
@@ -11,12 +11,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bytatech.ayoos.client.dms_core.api.SitesApi;
+import com.bytatech.ayoos.client.dms_core.model.SiteBodyCreate;
+import com.bytatech.ayoos.client.dms_core.model.SiteBodyCreate.VisibilityEnum;
+import com.bytatech.ayoos.client.dms_core.model.SiteEntry;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 @RestController
 @RequestMapping("/api")
 public class CommandResource {
+	
+	@Autowired
+	SitesApi siteApi;
+	
 	@GetMapping("/")
 	public String test() {
 		return "success";
 		}
+	@PostMapping("/site/{siteId}")
+	public String createSite(@PathVariable String siteId) {
+		SiteBodyCreate siteBodyCreate = new SiteBodyCreate();
+		siteBodyCreate.setTitle(siteId);
+		siteBodyCreate.setId(siteId);
+		siteBodyCreate.setVisibility(VisibilityEnum.MODERATED);
+		ResponseEntity<SiteEntry> entry = siteApi.createSite(siteBodyCreate, false, false, new ArrayList());
+		return entry.getBody().getEntry().getId();
+	}
+	
+	
+	
 }
