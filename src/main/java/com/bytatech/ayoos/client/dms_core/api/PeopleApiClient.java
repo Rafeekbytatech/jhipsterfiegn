9330a20c6ff64153280661d1fa86773595659ea2
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-10-04 15:05:05 +0530

    people

--- src/main/java/com/bytatech/ayoos/client/dms_core/api/PeopleApiClient.java
+++ src/main/java/com/bytatech/ayoos/client/dms_core/api/PeopleApiClient.java
@@ -3,6 +3,6 @@ package com.bytatech.ayoos.client.dms_core.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.bytatech.ayoos.client.dms_core.ClientConfiguration;
 
-@FeignClient(name="${dmsCore.name:dmsCore}", url="${dmsCore.url:http://localhost/alfresco/api/-default-/public/alfresco/versions/1}", configuration = ClientConfiguration.class)
+@FeignClient(name="${dmsCore.name:dmsCore}", url="${dmsCore.url:https://i0rezn.trial.alfresco.com/alfresco/api/-default-/public/alfresco/versions/1}", configuration = ClientConfiguration.class)
 public interface PeopleApiClient extends PeopleApi {
 }
