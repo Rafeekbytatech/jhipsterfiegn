f52c4d1dfbac18930ef520dbd78574d4cf88c41b
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-10-10 16:29:15 +0530

    add13

--- src/main/java/com/bytatech/ayoos/client/custom_dms_core/api/FlightClient.java
+++ src/main/java/com/bytatech/ayoos/client/custom_dms_core/api/FlightClient.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,7 +40,7 @@ public interface FlightClient {
     @RequestMapping(value = "/people",
             produces = "application/json", 
             method = RequestMethod.GET)
-    ResponseEntity<PersonPaging> listPeople();
+    ResponseEntity<PersonPaging> listPeople(@RequestHeader("Authorization") String token);
     
     
     @RequestMapping(value = "/sites",
